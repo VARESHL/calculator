474bc60e4a8f7882c32a8666a319935e93876a56
Glitch (veil-flaxen-patch) <none>
2024-02-14 17:27:55 +0000

    🕡🍲 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", addListeners);
+
+function addListeners() {
+  document.getElementById("btnGradecalc").addEventListener("click", ToGradecalc);
+  document.getElementById("btnSearchEg").addEventListener("click", Quest);
+}
+
+function ToGradecalc() {
+  window.location.href = "https://tricolor-pinto-barberry.glitch.me";
+}
+
+function Quest() {
+  window.location.href = "https://dorian-sage-fabrosaurus.glitch.me";
+
+  
